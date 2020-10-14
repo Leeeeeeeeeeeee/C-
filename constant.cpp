176f60e424e20f47dec3de5868c45d0635226125
qing1998 <39555742+Leeeeeeeeeeeee@users.noreply.github.com>
2020-10-15 00:34:40 +0800

    Delete constant.cpp

--- constant.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-  cout << ".145:\t" <<.145 << endl;
-  cout << "2.145E-1\t" << 2.145E-1 << endl;
-  cout << "2.145e2:\t" << 2.145e2 << endl;
-
-  return 0;
-}
